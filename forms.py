3aaa4e4eaa303f9af9ba9834b859d52bcc095fb7
samondev <s_monfared1@yahoo.com>
2023-07-19 00:46:08 +0300

    Backend database anf forms complete

---
+++ forms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
+from flask_ckeditor import CKEditorField
+
+class UserLogInForm(FlaskForm):
+    email = StringField("Email:", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Log In")
+
+class UserRegisterationForm(FlaskForm):
+    email = StringField("Email:", validators=[DataRequired()])
+    password = PasswordField("Password:", validators=[DataRequired()])
+    name = StringField("Name:", validators=[DataRequired()])
+    submit = SubmitField("Register")
+    
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title:", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    img_yrl = StringField("Blog Image URL:", validators=[DataRequired()])
+    body = StringField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+    
+class CommentForm(FlaskForm):
+    comment = CKEditorField("Your Comment", validators=[DataRequired()])
+    submit = SubmitField("Submit")
--- main.py
+++ main.py
@@ -1,8 +1,68 @@
 from flask import Flask, redirect, render_template, url_for
-
+from flask_bootstrap import Bootstrap
+from flask_ckeditor import CKEditor
+from datetime import date
+from functools import wraps
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from forms import UserLogInForm, UserRegisterationForm, CreatePostForm, CommentForm
+from flask_gravatar import Gravatar
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "c7e606b42bcce59a270a1c15e0d673da8cc953d44b546b1e59a53e245abfd03f"
+ckeditor = CKEditor(app)
+Bootstrap(app)
+gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, 
+                    force_lower=False, use_ssl=False, base_url=None)
+
+# DB Connection
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user():
+    return User.query.get(int(user_id))
+
+# DB Configuration
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(100))
+    posts = relationship('Posts', back_populates='author')
+    comments = relationship('Comments', back_populates='comment_author')
+    
+class Posts(db.Model):
+    __tablename__ = "posts"
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    author = relationship('User', back_populates='posts')
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    p_date = db.Column(db.String(100), nullable=False)
+    body = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+    comments = relationship('Comments', back_populates='parent_post')
+    
+class Comments(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    parent_post = relationship('Posts', back_populates='comments')
+    comment_author = relationship('User', back_populates='comments')
+    text = db.Column(db.Text, nullable=False)
+    
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
