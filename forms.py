b69bf51bcde45bdc09e2c3872580071f3303362d
samondev <s_monfared1@yahoo.com>
2023-08-05 19:56:19 +0300

    Page view accesss control and footer complete

--- forms.py
+++ forms.py
@@ -1,27 +1,33 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditorField
+
 
 class UserLogInForm(FlaskForm):
     email = StringField("Email:", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Log In")
 
+
 class UserRegisterationForm(FlaskForm):
     email = StringField("Email:", validators=[DataRequired()])
-    password = PasswordField("Password:", validators=[DataRequired()])
+    password = PasswordField("Password:", validators=[EqualTo('pass_conf',
+                                                              message="Password Missmatch")])
+    pass_conf = PasswordField("Password Confirm", validators=[DataRequired()])
     name = StringField("Name:", validators=[DataRequired()])
     submit = SubmitField("Register")
-    
+
+
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title:", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
     img_url = StringField("Blog Image URL:", validators=[DataRequired()])
     body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
-    
+
+
 class CommentForm(FlaskForm):
     comment = CKEditorField("Your Comment", validators=[DataRequired()])
     submit = SubmitField("Submit")
